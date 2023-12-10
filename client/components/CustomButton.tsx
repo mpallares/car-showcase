6b5413e9f66db8bce2f9d398f99477ade8050c27
Maria Pallarés <mariapallares@Marias-MBP.lan>
2023-12-10 22:22:40 +0000

    Initialise react next project, set up global styles, home page

---
+++ client/components/CustomButton.tsx
@@ -0,0 +1,26 @@
+'use client';
+import React, { MouseEventHandler } from 'react';
+import Image from 'next/image';
+
+interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type='button'
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
